db94a99043ceb19ef36fb6d49d92a872703f39c3
Seb Fuchs <sebastien@emmene-moi.fr>
2016-01-27 19:16:37 +0100

    fix multiple webroot facts and standalone command path + force 443 (usually free for bind)

--- manifests/exec/standalone.pp
+++ manifests/exec/standalone.pp
@@ -9,8 +9,9 @@ define letsencrypt::exec::standalone (
 
   $params_domain = join($domains, ' -d ')
   exec{ "letsencrypt-exec-standalone-${name}":
-    command  => "letsencrypt certonly -a standalone -d ${params_domain} --renew-by-default --server ${server}",
+    command  => "letsencrypt certonly -a standalone -d ${params_domain} --renew-by-default --standalone-supported-challenges tls-sni-01 --server ${server}",
     creates  => "/etc/letsencrypt/live/${domains[0]}/fullchain.pem",
-    require  => File['/etc/letsencrypt/cli.ini'];
+    require  => File['/etc/letsencrypt/cli.ini'],
+    path     => ['/usr/local/bin']
   }
 }
--- manifests/exec/webroot.pp
+++ manifests/exec/webroot.pp
@@ -16,29 +16,36 @@ define letsencrypt::exec::webroot (
       domains => $domains,
       server  => $server,
     }
-    # TODO FIXME: This fails if webroot is defined multiple times
-    file{ ['/etc/facter', '/etc/facter/facts.d']: ensure => directory; }
-    file{ '/etc/facter/facts.d/letsencrypt.txt':
-      content => 'letsencrypt_firstrun=SUCCESS',
-      owner   => root,
-      group   => root,
-      mode    => '0644',
-      require => Letsencrypt::Exec::Standalone[$name];
+    ensure_resource('file', ['/etc/facter', '/etc/facter/facts.d'], {'ensure' => 'directory' })
+    exec { "letsencrypt-fact-${name}-standalone":
+      command => "echo 'letsencrypt_firstrun=SUCCESS' > /etc/facter/facts.d/letsencrypt.txt",
+      creates => "/etc/facter/facts.d/letsencrypt.txt",
+      path => ["/usr/bin", "/usr/sbin"],
+      require => Letsencrypt::Exec::Standalone[$name]
+    } ->
+    exec { "letsencrypt-fact-${name}-standalone-rights":
+      command => "chmod 0644 /etc/facter/facts.d/letsencrypt.txt ; chown root:root /etc/facter/facts.d/letsencrypt.txt",
+      path => ["/usr/bin", "/usr/sbin"]
     }
+
   } else {
     if $letsencrypt::firstrun_webroot and $::letsencrypt_firstrun != 'SUCCESS'{
       $real_webroot = $letsencrypt::firstrun_webroot
-      # TODO FIXME: This fails if webroot is defined multiple times
-      file{ ['/etc/facter', '/etc/facter/facts.d']: ensure => directory; }
-      file{ '/etc/facter/facts.d/letsencrypt.txt':
-        content => 'letsencrypt_firstrun=SUCCESS',
-        owner   => root,
-        group   => root,
-        mode    => '0644',
-        require => Exec["letsencrypt-exec-webroot-${name}"],
+      ensure_resource('file', ['/etc/facter', '/etc/facter/facts.d'], {'ensure' => 'directory' })
+
+      exec { "letsencrypt-fact-${name}-webroot":
+        command => "echo 'letsencrypt_firstrun=SUCCESS' > /etc/facter/facts.d/letsencrypt.txt",
+        creates => "/etc/facter/facts.d/letsencrypt.txt",
+        path => ["/usr/bin", "/usr/sbin"],
+        require => Exec["letsencrypt-exec-webroot-${name}"]
+      } ->
+      exec { "letsencrypt-fact-${name}-webroot-rights":
+        command => "chmod 0644 /etc/facter/facts.d/letsencrypt.txt ; chown root:root /etc/facter/facts.d/letsencrypt.txt",
+        path => ["/usr/bin", "/usr/sbin"]
       }
+
     } else {
-    $real_webroot = $webroot
+      $real_webroot = $webroot
     }
     exec{ "letsencrypt-exec-webroot-${name}":
       command => "letsencrypt certonly -a webroot --webroot-path ${real_webroot} -d ${params_domain} --renew-by-default --server ${server}",
